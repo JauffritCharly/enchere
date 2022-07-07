9f36bd9eb42e6ddb9a1b6642cce270bacaa3b364
JauffritCharly <jauffrit.charly@hotmail.com>
2022-07-07 08:17:24 +0200

    Mise a jour class

---
+++ src/main/java/fr/afpa/enchere/bo/Utilisateur.java
@@ -0,0 +1,19 @@
+package fr.afpa.enchere.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Utilisateur {
+    private int idUtilisateur;
+    private String pseudo;
+    private String nom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private int ville;
+    private String motDePasse;
+    private int credit;
+    private boolean administrateur;
+}
