95058ae33cdf1650c584a8233f09d18d4b19e378
JauffritCharly <jauffrit.charly@hotmail.com>
2022-07-08 11:30:56 +0200

    mise en place session

--- src/main/java/fr/afpa/enchere/servlet/DeconnexionServlet.java
+++ src/main/java/fr/afpa/enchere/servlet/DeconnexionServlet.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class DeconnexionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        
         request.getRequestDispatcher("index.jsp").forward(request, response);
 
     }
